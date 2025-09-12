0be4af1790df69d83eebe4dc5650ed8c50bb0e82
clarasousadias <cso.dias@campus.fct.unl.pt>
2025-09-12 11:47:18 +0100

    EP command done! Needs improvement

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String EXISTS_PHONE  = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -24,6 +25,8 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String EXISTS_PHONENUMBER ="There are contacts that share phone numbers.";
+    public static final String NO_REPETITION= "All contacts have different phone numbers";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -53,6 +56,9 @@ public class Main {
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
                     break;
+                    case EXISTS_PHONE:
+                        exists_repetition(cBook);
+                        break;
                 default:
                     System.out.println(COMMAND_ERROR);
             }
@@ -63,6 +69,8 @@ public class Main {
         System.out.println();
         in.close();
     }
+
+
 
     private static String getCommand(Scanner in) {
         String input;
@@ -146,5 +154,11 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+    private static void exists_repetition(ContactBook cBook) {
+        if(cBook.ep())
+            System.out.println( EXISTS_PHONENUMBER);
+        else
+            System.out.println( NO_REPETITION);
     }
 }
